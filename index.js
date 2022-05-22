6b5de5876cdea657372fb2e5f2d38f84cb090530
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-22 20:17:12 +0530

    Logged in the bot

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ const opus = require("opusscript");
 const ffmpeg = require("ffmpeg");
 const ytsearch = require("yt-search");
 const yt = require("ytdl-core");
+
+/ Music Player
+const queue = new Map();
+
+// Command Handler
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+// Bot Login
+client.login(config.token);
+
+// Bot Status
+client.on("ready", () => {
+	console.log("Bot is online || Yeah Boi!(Sorry if you are a girl!)");
+	client.user.setPresence({
+		status: "online",
+		game: {
+			name: "with my friends",
+			type: "PLAYING"
+		}
+	});
+});
